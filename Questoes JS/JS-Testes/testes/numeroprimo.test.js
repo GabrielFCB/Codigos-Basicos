e72909c427c607f739a3ff014d9a1e40d49a1ae7
Gabriel <117751056+GabrielFCB@users.noreply.github.com>
2023-05-23 16:53:05 -0300

    Delete Questoes JS/JS-Testes directory

--- Questoes JS/JS-Testes/testes/numeroprimo.test.js
+++
@@ -1,9 +0,0 @@
-const num_primo = require('../algoritmos js/numeroprimo');
-
-test('O número 6 não deve ser um número primo', () => {
-    expect(num_primo(6)).toBe(false);
-});
-
-test('O número 23  deve ser um número primo', () => {
-    expect(num_primo(23)).toBe(true);
-});
